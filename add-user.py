0399eba04736414fcfe06f8cfa5883dd85fee1c3
c-cal <github-2c7c@zebrina.fr>
2021-03-19 00:07:22 +0100

    feat: add tools for manage users

---
+++ add-user.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+# usage: ./add-user.sh <username> <password> [<database>]
+
+from hashlib import blake2b
+import sqlite3
+import sys
+
+username = sys.argv[1]
+password = sys.argv[2]
+path = sys.argv[3] if len(sys.argv) > 3 else "conference-mapper.db"
+
+password_hash = blake2b(password.encode()).hexdigest()
+
+connection = sqlite3.connect(path)
+connection.execute(
+    "INSERT INTO user (name, password_hash) VALUES (?,?)", (username, password_hash)
+)
+connection.commit()
+connection.close()
