ef1d9449acccc3b182df8f07123ee5960d9cbf52
c-cal <github-2c7c@zebrina.fr>
2022-10-24 10:36:04 +0200

    refactor: move scripts

--- scripts/add-user.py
+++ scripts/add-user.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
 # usage: ./add-user.sh <username> <password> [<database>]
 
--- scripts/del-user.sh
+++ scripts/del-user.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 # usage: ./del-user.sh <username> [<database>]
 
